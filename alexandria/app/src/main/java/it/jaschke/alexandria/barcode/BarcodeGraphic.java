36bf26e417647a5281aea6d786e5162dad940117
concaveNP <concave@acm.org>
2016-03-26 17:29:14 -0600

    Did the wrong work on this branch so far.  Was fixing comments and such instead of figuring out the intent of the ticket.

--- alexandria/app/src/main/java/it/jaschke/alexandria/barcode/BarcodeGraphic.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/barcode/BarcodeGraphic.java
@@ -27,6 +27,14 @@ import it.jaschke.alexandria.barcode.ui.camera.GraphicOverlay;
 /**
  * Graphic instance for rendering barcode position, size, and ID within an associated graphic
  * overlay view.
+ *
+ * NOTE: This code was taken from the Google Vision samples code that demonstrated the capabilities
+ * of the barcode scanning capabilities.  I've left the code untouched as much as possible.
+ *
+ * References:
+ * - The Mobile Vision web site: https://developers.google.com/vision/
+ * - The GitHub project that hosted the code used here: https://github.com/googlesamples/android-vision
+ *      - NOTE: barcode-reader was the name of the visionSample within the repo that was used
  */
 public class BarcodeGraphic extends GraphicOverlay.Graphic {
 
